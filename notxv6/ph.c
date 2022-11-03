a8d6119279eda69399b3465b53c14e21e53dd81d
ubuntu <ubuntu@localhost.localdomain>
2022-11-03 11:45:29 +0800

    lab5

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t locks[NBUCKET];
 double
 now()
 {
@@ -51,7 +51,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&locks[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&locks[i]); 
   }
 }
 
@@ -114,7 +116,9 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  for(int i=0;i<NBUCKET;++i){
+    pthread_mutex_init(&locks[i], NULL);
+  }
   //
   // first the puts
   //
